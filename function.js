3eb8efbb366cb59e1d5f253fc20826d9e7e65e43
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-10-23 14:08:12 +0530

    add new file

---
+++ function.js
@@ -0,0 +1,21 @@
+// Write a Function to Add Two Numbers
+function addtwoNumber(x,y){
+    return x+y;
+}
+console.log(addtwoNumber(2,3));
+
+// Write a Function to Check Even or Odd
+const number = 8;
+function evenOdd(number) {
+    if (number % 2 === 0) {
+        console.log("This is Even Number");
+    } else {
+        console.log("This is Odd Number");
+    }
+}
+console.log(evenOdd(number));
+
+
+
+
+
